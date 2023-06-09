f5328735b2cbf7531666b7a5a7e1c3dbc777ad23
Lakshmidevi2002 <124182437+Lakshmidevi2002@users.noreply.github.com>
2023-06-09 18:10:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4QUKASHDSEKLAZKU"
-  secret_key = "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"
+  access_key = "AKIAXBOE7TRT6D5PSBUG"
+  secret_key = "JWs+EdrsLysjr5CwB4Hecyo5UK2FAUkrFYPCRkLI"
 }
-
 resource "aws_instance" "example" {
-  ami           = "ami-0e6329e222e662a52"
+  ami           = "ami-"
   instance_type = var.instance_type
   tags= {
-    Name = "raham"
+    Name = "chinni"
   }
 }
 
